3b5a107e2733f677525cc21a6eb66bc81565467a
Navadeep <navdeep2666@gmail.com>
2025-10-14 23:29:38 +0530

    first commit

---
+++ src/App.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    public void start(Stage stage){
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+            Scene scene = new Scene(root);
+            //scene1.getStylesheets().add(getClass().getResource("Scene1.css").toExternalForm());
+            stage.setScene(scene);
+            stage.show();
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
