070c863b32b0f42f7a6d86287e6dee0a6cb91ad2
Navadeep <navdeep2666@gmail.com>
2025-10-28 23:30:23 +0530

    home page designing

--- src/controllers/homeController.java
+++ src/controllers/homeController.java
@@ -1,13 +1,44 @@
 package controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 
 public class homeController {
 
     @FXML
+    private Button addEntry;
+
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
     private Label titlelabel;
-    
-    
+
+    @FXML
+    private Button viewEntries;
+
+    @FXML
+    private Label welcome;
+
+    @FXML
+    void addEn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewEn(ActionEvent event) {
+
+    }
 
 }
